569ef7232ef6a58359275747d3723491e9992519
agudelotmateo <agudelotmateo@gmail.com>
2018-04-17 09:51:50 -0500

    MRJob Punto 1 - Empleados

---
+++ mrjob/empleados/empleados-mr-1.py
@@ -0,0 +1,18 @@
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        (sector, _, salario, _) = line.split(',')
+        yield sector, int(salario)
+
+    def reducer(self, key, values):
+        accum = 0
+        length = 0
+        for v in values:
+            accum += v
+            length += 1
+        yield key, accum/length
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
---
+++ mrjob/empleados/empleados-mr-3.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        (sector, empleado, _, _) = line.split(',')
+        yield empleado, sector
+
+    def reducer(self, key, values):
+        s = set()
+        for v in values:
+            s.add(v)
+        yield key, len(s)
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
